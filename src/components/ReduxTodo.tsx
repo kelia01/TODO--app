5e6da98e12288e56194a235d58c61c705dc33ea6
Kelia Iradukunda <iradukundakelia@gmail.com>
2025-09-02 06:11:41 +0200

    Create a todo with redux

---
+++ src/components/ReduxTodo.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import { UseDispatch } from 'react-redux';
+import {addTodo} from '../redux/todoSlice';
+
+function ReduxTodo() {
+  return (
+    <div>ReduxTodo</div>
+  )
+}
+
+export default ReduxTodo
